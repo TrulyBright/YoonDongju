240fff1df15a63c066c297a99f4d68dd88668ee8
TrulyBright <trulybright@yonsei.ac.kr>
2021-11-25 09:22:34 +0900

    Index/404 page done; replace Ki with Jeong.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask
 import db
 import routes
+import middlewares
 
 db.initalize("notices")
 db.initalize("users")
 app = Flask("YoonDong-ju", static_url_path="/static")
 app.config["UPLOAD_DIR"] = "./uploaded" # "/uploaded"가 아니라 "./uploaded"임에 유의.
 routes.setup_routes(app)
+middlewares.add_middlewares(app)
 
 if __name__ == "__main__": # 로컬에서 실행
     app.run()
---
+++ middlewares.py
@@ -0,0 +1,7 @@
+from flask import Flask, redirect, render_template
+
+def add_middlewares(app:Flask):
+    app.register_error_handler(404, handle_404)
+
+def handle_404(e) -> str:
+    return render_template("404.html"), 404
