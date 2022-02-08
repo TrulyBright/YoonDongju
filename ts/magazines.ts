b25ecc37fc20cc2eb8247d61e6033d055f747909
TrulyBright <trulybright@yonsei.ac.kr>
2022-02-09 06:08:03 +0900

    toggle magazine content info by button

--- ts/magazines.ts
+++ ts/magazines.ts
@@ -1,7 +1,11 @@
 document.querySelectorAll(".item").forEach((element)=>{
     const info = element.parentNode.querySelector(".content-info") as HTMLElement;
-    element.addEventListener("click", (event)=>{
+    element.querySelector("img").addEventListener("click", (event)=>{
         info.style.display = "block";
+        element.classList.toggle("hidden");
+    });
+    element.parentNode.querySelector(".hide-content").addEventListener("click", (event)=>{
+        info.style.removeProperty("display");
         element.classList.toggle("hidden");
     });
 });
