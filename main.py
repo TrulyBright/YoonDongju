c645a8637994d40bd6b204495035da5ab898eda0
TrulyBright <trulybright@yonsei.ac.kr>
2022-02-09 03:06:20 +0900

    fix: can't fetch club info at vanilla

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import middlewares
 db.initalize()
 app = Flask("YoonDong-ju", static_url_path="/static")
 app.config["UPLOAD_DIR"] = "./uploaded" # "/uploaded"가 아니라 "./uploaded"임에 유의.
-routes.setup_routes(app)
 middlewares.add_middlewares(app)
 auth.setup_auth(app)
+routes.setup_routes(app)
 
 if __name__ == "__main__": # 로컬에서 실행
     app.run(debug=True)
