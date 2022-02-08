13ec28976846f4d9c6e4fa222c6b8413676326b5
TrulyBright <trulybright@yonsei.ac.kr>
2022-02-09 00:42:08 +0900

    handle 401

--- middlewares.py
+++ middlewares.py
@@ -5,6 +5,7 @@ def add_middlewares(app:Flask):
     app.register_error_handler(404, handle_404)
     app.register_error_handler(500, handle_500)
     app.register_error_handler(403, handle_403)
+    app.register_error_handler(401, handle_401)
 
 def handle_404(e):
     return render_template("error.html", code=404), 404
@@ -14,3 +15,6 @@ def handle_500(e):
 
 def handle_403(e):
     return render_template("error.html", code=403), 403
+
+def handle_401(e):
+    return render_template("error.html", code=401), 401
