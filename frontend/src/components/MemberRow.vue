7766da60c280a528eb6ca471aba4ab6c392e79df
trulybright <trulybright@yonsei.ac.kr>
2022-04-29 12:34:51 +0900

    member list

---
+++ frontend/src/components/MemberRow.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  props: {
+    username: String,
+    real_name: String,
+    student_id: Number,
+    role: String,
+  },
+  computed: {
+    roleInKorean() {
+      switch (this.role) {
+        case "member":
+          return "회원";
+        case "board":
+          return "임원";
+        case "president":
+          return "회장";
+        default:
+          return "불명";
+      }
+    },
+  },
+};
+</script>
+<template>
+  <div>{{ username }}</div>
+  <div>{{ real_name }}</div>
+  <div>{{ student_id }}</div>
+  <div>{{ roleInKorean }}</div>
+  <div>
+    <button type="button">권한 변경</button>
+  </div>
+</template>
+<style scoped></style>
---
+++ frontend/src/views/AdminMemberList.vue
@@ -0,0 +1,28 @@
+<script setup>
+import axios from "axios";
+import { useMemberStore } from "../stores/member";
+import MemberRow from "../components/MemberRow.vue";
+</script>
+<script>
+const store = useMemberStore();
+export default {
+  data() {
+    return {
+      list: [],
+    };
+  },
+  async created() {
+    const response = await axios.get("members", {
+      headers: {
+        Authorization: store.authorizationHeader,
+      },
+    });
+    this.list = response.data;
+  },
+};
+</script>
+<template>
+  <h2>회원 관리</h2>
+  <MemberRow v-for="m in list" v-bind="m" :key="m.student_id"></MemberRow>
+</template>
+<style scoped></style>
