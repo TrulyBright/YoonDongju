99160119d3023ba9e261869460e2c300d173ca96
trulybright <trulybright@yonsei.ac.kr>
2022-05-01 01:39:37 +0900

    bootstrap

--- frontend/src/components/LoginModal.vue
+++ frontend/src/components/LoginModal.vue
@@ -22,20 +22,20 @@ export default {
 };
 </script>
 <template>
-  <form @submit.prevent="submit">
-    <button type="button" @click="$emit('close')">×</button>
-    <div>
-      <h1>로그인</h1>
-    </div>
-    <div>
-      <input type="text" v-model="form.username" placeholder="ID" />
-      <input type="password" v-model="form.password" placeholder="비밀번호" />
-      <input type="submit" value="접속" />
-    </div>
+  <BForm @submit.prevent="submit">
+    <BButton @click="$emit('close')">×</BButton>
+    <h1>로그인</h1>
+    <BFormInput type="text" v-model="form.username" placeholder="ID" />
+    <BFormInput
+      type="password"
+      v-model="form.password"
+      placeholder="비밀번호"
+    />
+    <BButton type="submit">접속</BButton>
     <div>
       <a>ID를 잊어버렸다면</a>
       <a>비밀번호를 잊어버렸다면</a>
     </div>
-  </form>
+  </BForm>
 </template>
 <style></style>
