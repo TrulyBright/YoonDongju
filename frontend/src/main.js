7915879588545a5000a4264866e281e39ff53c6a
trulybright <trulybright@yonsei.ac.kr>
2022-07-05 04:53:37 +0900

    Bootstrap 5

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,8 +6,8 @@ import App from "./App.vue";
 import router from "./router";
 import BootstrapVue3 from "bootstrap-vue-3";
 
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = "http://localhost:8000/";
