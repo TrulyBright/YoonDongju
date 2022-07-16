a4f7780ad999b04481ac400c0797cefdbe7e42e2
trulybright <trulybright@yonsei.ac.kr>
2022-07-17 07:03:34 +0900

    gap between sections on home

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -21,71 +21,79 @@ export default {
 };
 </script>
 <template>
-  <img
-    src="@/assets/대문-그림.jpg"
-    class="img-fluid"
-    id="front-image"
-    alt="연세문학회 문집 더미에 파묻힌 강아지가 바깥으로 몸을 반쯤 내밀고 숨을 돌리고 있다."
-  />
-  <div>
-    <h3>
-      <strong><RouterLink to="notices">공지</RouterLink></strong>
-    </h3>
-    <div class="recent-notices">
-      <div v-for="notice in recentNotices" :key="notice" class="recent-notice">
-        <RouterLink :to="{ name: 'notice', params: { no: notice.no } }">{{
-          notice.title
-        }}</RouterLink>
-        <div>{{ notice.published }}</div>
-      </div>
+  <div class="home">
+    <div class="container-sm" id="front-image-container">
+      <img
+        src="@/assets/대문-그림.jpg"
+        class="img-fluid"
+        id="front-image"
+        alt="연세문학회 문집 더미에 파묻힌 강아지가 바깥으로 몸을 반쯤 내밀고 숨을 돌리고 있다."
+      />
     </div>
-  </div>
-  <div>
-    <h3>
-      <strong><RouterLink to="magazines">문집</RouterLink></strong>
-    </h3>
-    <div
-      id="carouselExampleInterval"
-      class="carousel carousel-dark slide"
-      data-bs-ride="carousel"
-    >
-      <div class="carousel-inner">
+    <div>
+      <h3>
+        <strong><RouterLink to="notices">공지</RouterLink></strong>
+      </h3>
+      <div class="recent-notices">
         <div
-          :class="'carousel-item' + (index === 0 ? ' active' : '')"
-          v-for="(magazine, index) in recentMagazines"
-          :key="magazine"
-          data-bs-interval="5000"
+          v-for="notice in recentNotices"
+          :key="notice"
+          class="recent-notice"
         >
-          <img
-            :src="axios.defaults.baseURL + 'uploaded/' + magazine.cover"
-            class="img-fluid"
-            :alt="magazine.published + '에 발간된 문집 표지'"
-          />
+          <RouterLink :to="{ name: 'notice', params: { no: notice.no } }">{{
+            notice.title
+          }}</RouterLink>
+          <div>{{ notice.published }}</div>
         </div>
       </div>
-      <button
-        class="carousel-control-prev"
-        type="button"
-        data-bs-target="#carouselExampleInterval"
-        data-bs-slide="prev"
+    </div>
+    <div>
+      <h3>
+        <strong><RouterLink to="magazines">문집</RouterLink></strong>
+      </h3>
+      <div
+        id="carouselExampleInterval"
+        class="carousel carousel-dark slide"
+        data-bs-ride="carousel"
       >
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">전</span>
-      </button>
-      <button
-        class="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselExampleInterval"
-        data-bs-slide="next"
-      >
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">후</span>
-      </button>
+        <div class="carousel-inner">
+          <div
+            :class="'carousel-item' + (index === 0 ? ' active' : '')"
+            v-for="(magazine, index) in recentMagazines"
+            :key="magazine"
+            data-bs-interval="5000"
+          >
+            <img
+              :src="axios.defaults.baseURL + 'uploaded/' + magazine.cover"
+              class="img-fluid"
+              :alt="magazine.published + '에 발간된 문집 표지'"
+            />
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleInterval"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">전</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleInterval"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">후</span>
+        </button>
+      </div>
     </div>
   </div>
 </template>
 <style scoped>
-#front-image {
+#front-image-container {
   border-radius: 30px;
   padding: 1em;
   background: white;
@@ -95,5 +103,9 @@ export default {
   flex-direction: row;
   justify-content: space-between;
   border-bottom: 1px solid brown;
+}
+.home {
+  display: grid;
+  gap: 15px;
 }
 </style>
