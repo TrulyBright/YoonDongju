b5bf1e08bc7a9b5e4a00f179eb1dffbaa1acd8a2
trulybright <trulybright@yonsei.ac.kr>
2022-07-17 05:47:17 +0900

    icon for attached files

--- frontend/src/components/PostPreview.vue
+++ frontend/src/components/PostPreview.vue
@@ -12,7 +12,26 @@ export default {
   <div>
     <h1>{{ form.title }}</h1>
     <Markdown class="markdown" :source="form.content"></Markdown>
-    <div>{{ form.attached }}</div>
+    <hr />
+    <div>
+      <small>첨부파일</small>
+      <ul v-if="form.attached" class="list-group list-group-flush">
+        <li v-for="file in form.attached" :key="file" class="list-group-item">
+          <small
+            ><i
+              :class="
+                'bi-filetype-' +
+                file.name
+                  .split('.')
+                  [file.name.split('.').length - 1].toLowerCase()
+              "
+            ></i
+            >{{ file.name }}</small
+          >
+        </li>
+      </ul>
+      <div v-else>첨부된 파일이 없습니다.</div>
+    </div>
   </div>
 </template>
 <style>
