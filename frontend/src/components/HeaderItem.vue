3c33448bac692c562080ded82138413a6417251f
trulybright <trulybright@yonsei.ac.kr>
2022-07-17 03:42:39 +0900

    apply form floating to register modal

--- frontend/src/components/HeaderItem.vue
+++ frontend/src/components/HeaderItem.vue
@@ -170,17 +170,22 @@ export default {
         <div class="modal-body">
           <p>동아리 회원가입은 따로 하셔야 합니다.</p>
           <form @submit="submit">
-            <div class="mb-3">
+            <div class="form-floating">
               <input
                 type="text"
                 class="form-control"
+                id="portal-id"
                 v-model="registerForm.portal_id"
                 placeholder="연세포탈 ID"
                 required
               />
+              <label for="portal-id">연세포탈 ID</label>
+            </div>
+            <div class="form-floating">
               <input
                 type="password"
                 class="form-control"
+                id="portal-pw"
                 v-model="registerForm.portal_pw"
                 placeholder="연세포탈 비밀번호"
                 data-bs-toggle="tooltip"
@@ -188,16 +193,24 @@ export default {
                 title="저장되지 않고, 신촌캠 구성원 확인에만 일회용됩니다."
                 required
               />
+              <label for="portal-pw">연세포탈 비밀번호</label>
+            </div>
+            <div class="form-floating">
               <input
                 type="text"
                 class="form-control"
+                id="real-name"
                 v-model="registerForm.real_name"
                 placeholder="실명"
                 required
               />
+              <label for="real-name">실명</label>
+            </div>
+            <div class="form-floating">
               <input
                 type="text"
                 class="form-control"
+                id="username"
                 v-model="registerForm.username"
                 placeholder="사용할 계정명 (ID)"
                 pattern="^.{1,65}$"
@@ -206,9 +219,13 @@ export default {
                 data-bs-placement="bottom"
                 title="최대 64자에, 한글을 비롯하여 어떤 문자든 허용됩니다."
               />
+              <label for="username">사용할 계정명 (ID)</label>
+            </div>
+            <div class="form-floating">
               <input
                 type="password"
                 class="form-control"
+                id="password"
                 v-model="registerForm.password"
                 placeholder="사용할 비밀번호"
                 pattern="^(?=.*[0-9])(?=.*[a-zA-Z]).{10,}$"
@@ -217,13 +234,18 @@ export default {
                 title="10자 이상에 숫자와 영문이 하나씩은 있어야 합니다."
                 required
               />
+              <label for="password">사용할 비밀번호</label>
+            </div>
+            <div class="form-floating">
               <input
                 type="password"
                 class="form-control"
+                id="password-confirm"
                 placeholder="사용할 비밀번호 재입력"
                 v-model="registerForm.passwordConfirm"
                 required
               />
+              <label for="password-confirm">사용할 비밀번호 재입력</label>
             </div>
           </form>
         </div>
