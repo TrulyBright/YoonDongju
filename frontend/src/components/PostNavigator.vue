ea58aa83b36329cf6bd5a879ef5e16dd4e2d60be
trulybright <trulybright@yonsei.ac.kr>
2022-07-20 14:14:01 +0900

    lint

--- frontend/src/components/PostNavigator.vue
+++ frontend/src/components/PostNavigator.vue
@@ -2,15 +2,18 @@
 export default {
   props: {
     text: String,
-  }
-}
+  },
+};
 </script>
 <template>
   <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
-      <template v-for="stop in typeof $route.meta.breadCrumb === 'function'
-        ? $route.meta.breadCrumb(text)
-        : $route.meta.breadCrumb" :key="stop">
+      <template
+        v-for="stop in typeof $route.meta.breadCrumb === 'function'
+          ? $route.meta.breadCrumb(text)
+          : $route.meta.breadCrumb"
+        :key="stop"
+      >
         <RouterLink class="breadcrumb-item" :to="stop.to" v-if="'to' in stop">
           {{ stop.text }}</RouterLink
         >
