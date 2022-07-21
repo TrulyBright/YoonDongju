fe9b7a5ec2741c8d58c92ef100be39a88022ffa3
trulybright <trulybright@yonsei.ac.kr>
2022-07-21 22:09:44 +0900

    PostOutline Design Complete

--- frontend/src/components/PostOutlineItem.vue
+++ frontend/src/components/PostOutlineItem.vue
@@ -13,14 +13,20 @@ export default {
 </script>
 
 <template>
-  <div class="item d-flex justify-content-between">
-    <div class="d-none d-lg-block">{{ no }}</div>
-    <RouterLink :to="{ name: 'notice', params: { no: no } }">{{
-      title
-    }}</RouterLink>
-    <div class="d-none d-lg-block">{{ author }}</div>
-    <small class="published d-lg-none align-self-center">{{ published.split("-").slice(1,).join("-") }}</small>
-    <small class="published d-none d-lg-block align-self-center">{{ published }}</small>
+  <div class="container">
+    <div class="row text-center">
+      <div class="col col-lg-auto d-none d-lg-block">{{ no }}</div>
+      <RouterLink class="col" :to="{ name: 'notice', params: { no: no } }">{{
+        title
+      }}</RouterLink>
+      <div class="col col-lg-auto d-none d-lg-block">{{ author }}</div>
+      <small class="col col-lg-auto d-lg-none align-self-center">{{
+        published.split("-").slice(1).join("-")
+      }}</small>
+      <small class="col col-lg-auto d-none d-lg-block align-self-center">{{
+        published
+      }}</small>
+    </div>
   </div>
 </template>
 
