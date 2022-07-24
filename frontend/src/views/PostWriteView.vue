c049ada41e5ad99ebb282f62856dbb7d62a23a12
trulybright <trulybright@yonsei.ac.kr>
2022-07-24 17:34:33 +0900

    fix: prop type checking fail

--- frontend/src/views/PostWriteView.vue
+++ frontend/src/views/PostWriteView.vue
@@ -9,6 +9,6 @@ export default {
 };
 </script>
 <template>
-  <PostWrite :type="type" :no="$route.query.no"></PostWrite>
+  <PostWrite :type="type" :no="Number($route.query.no)"></PostWrite>
 </template>
 <style></style>
