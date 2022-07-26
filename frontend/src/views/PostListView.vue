6d32ecc85ca769783d829043501667798bc8d3c4
trulybright <trulybright@yonsei.ac.kr>
2022-07-26 10:27:35 +0900

    Pagination complete (really)

--- frontend/src/views/PostListView.vue
+++ frontend/src/views/PostListView.vue
@@ -13,94 +13,82 @@ export default {
     return {
       items: [],
       rowPerPage: 10, // 한번에 보이는 post 수
-      thereAreMore: false, // >> 버튼을 활성화해야 하는 경우
+      total: 0,
     };
   },
-  watch: {
-    async leftestIndex(newOffset, oldOffset) {
-      await this.getRows();
-    },
+  async created() {
+    const response = await axios.get("notice-count");
+    this.total = response.data;
+    await this.getRows();
   },
   methods: {
     async getRows() {
-      try {
-        const result = await axios.get(
-          `/${this.type}?skip=${
-            Math.floor(this.offset / this.limit) * this.limit
-          }&limit=${this.limit}`
-        );
-        this.items = result.data;
-        const rest = await axios.get(
-          `/${this.type}?skip=${
-            Math.max(this.rightestIndex, 4) * this.rowPerPage
-          }&limit=1`
-        );
-        this.thereAreMore = rest.data.length !== 0;
-      } catch (error) {
-        console.error(error);
-      }
-      console.log(
-        this.offset,
-        this.limit,
-        this.paginationSpan,
-        this.currentIndex,
-        this.leftestIndex,
-        this.rightestIndex
+      const itemsResponse = await axios.get(
+        `notices?skip=${this.offset}&limit=${this.rowPerPage}`
       );
-      console.log(this.items);
+      this.items = itemsResponse.data;
     },
   },
-  async created() {
-    await this.getRows();
+  watch: {
+    async offset() {
+      await this.getRows();
+    },
   },
   computed: {
+    totalPages() {
+      return Math.ceil(
+        (this.total - this.left * this.rowPerPage) / this.rowPerPage
+      );
+    },
+    currentPage() {
+      return Math.floor(this.offset / this.rowPerPage);
+    },
+    width() {
+      return 5;
+    },
+    left() {
+      return Math.max(
+        0,
+        Math.floor(this.offset / (this.rowPerPage * this.width)) * this.width
+      );
+    },
+    generatePages() {
+      return this.totalPages < this.width
+        ? [...new Array(this.totalPages).keys()].map((item) => item + this.left)
+        : [...new Array(this.width).keys()].map(
+            (value, index) => index + this.left
+          );
+    },
+    prevMore() {
+      return this.generatePages[0] > 0;
+    },
+    nextMore() {
+      return (
+        this.generatePages[this.generatePages.length - 1] - this.left <
+        this.totalPages - 1
+      );
+    },
+    typeToRequest() {
+      switch (this.type) {
+        case "notices":
+        case "notice":
+          return "notices";
+        default:
+          throw "Type unknown to PostListView: " + this.type;
+      }
+    },
     offset() {
       return this.$route.query.skip || 0;
-    },
-    limit() {
-      return this.$route.query.limit || 50;
-    },
-    paginationSpan() {
-      // pagination 가로줄 길이 (=칸 개수)
-      return Math.floor(this.limit / this.rowPerPage);
-    },
-    // all below are 1-based index.
-    currentIndex() {
-      // 현재 보고 있는 pagination 칸 번호
-      return Math.floor(this.offset / this.rowPerPage) + 1;
-    },
-    leftestIndex() {
-      // pagination 가로줄의 맨 왼쪽 칸 번호
-      return Math.floor(this.offset / this.limit) * this.paginationSpan + 1;
-    },
-    rightestIndex() {
-      // pagination 가로줄의 맨 오른쪽 칸 번호
-      return (
-        this.leftestIndex -
-        1 +
-        Math.min(
-          Math.ceil(this.items.length / this.rowPerPage),
-          this.paginationSpan
-        )
-      );
     },
   },
 };
 </script>
 <template>
-  <main :key="items">
+  <main>
     <PostNavigator></PostNavigator>
-    <ul class="list-group rounded">
+    <ul class="list-group rounded" :key="currentPage">
       <PostOutlineItem
-        v-for="item in items.slice(
-          ((currentIndex % (paginationSpan + 1)) -
-            1 +
-            Math.floor(leftestIndex / paginationSpan)) *
-            rowPerPage,
-          ((currentIndex % (paginationSpan + 1)) +
-            Math.floor(leftestIndex / paginationSpan)) *
-            rowPerPage
-        )"
+        v-for="item in items"
         v-bind="item"
         :key="item"
         class="list-group-item"
@@ -109,51 +97,31 @@ export default {
     <ListAction :type="type"></ListAction>
     <nav aria-label="Page navigation example">
       <ul class="pagination pagination-sm justify-content-center">
-        <li
-          :class="'page-item' + (leftestIndex === 1 ? ' disabled' : '')"
-          :tabindex="leftestIndex === 1 ? '-1' : '0'"
-        >
+        <li v-if="prevMore" class="page-item">
           <RouterLink
             class="page-link"
             :to="{
               path: 'notices',
-              query: {
-                skip: ((leftestIndex - 1) / paginationSpan - 1) * limit,
-              },
+              query: { skip: offset - rowPerPage * width },
             }"
-            aria-label="Previous"
           >
             <span aria-hidden="true">&laquo;</span>
           </RouterLink>
         </li>
-        <li
-          :class="
-            'page-item' +
-            (leftestIndex + index === currentIndex ? ' active' : '')
-          "
-          v-for="index in [...Array(rightestIndex - leftestIndex + 1)].keys()"
-          :key="index"
-        >
+        <li v-for="page in generatePages" :key="page">
           <RouterLink
-            class="page-link"
-            :to="{
-              path: 'notices',
-              query: { skip: (leftestIndex - 1 + index) * rowPerPage },
-            }"
-            >{{ leftestIndex + index }}</RouterLink
+            :class="'page-link' + (currentPage === page ? ' active' : '')"
+            :to="{ path: 'notices', query: { skip: rowPerPage * page } }"
+            >{{ page + 1 }}</RouterLink
           >
         </li>
-        <li
-          :class="'page-item' + (thereAreMore ? '' : ' disabled')"
-          :tabindex="thereAreMore ? '-1' : '0'"
-        >
+        <li v-if="nextMore" class="page-item">
           <RouterLink
             class="page-link"
             :to="{
               path: 'notices',
-              query: { skip: rightestIndex * rowPerPage },
+              query: { skip: (left + width) * rowPerPage },
             }"
-            aria-label="Next"
           >
             <span aria-hidden="true">&raquo;</span>
           </RouterLink>
