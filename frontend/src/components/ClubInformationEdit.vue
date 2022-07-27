3eadfa8fc963c5249626b7d214bced1713e2642e
trulybright <trulybright@yonsei.ac.kr>
2022-07-27 22:12:01 +0900

    floating form

--- frontend/src/components/ClubInformationEdit.vue
+++ frontend/src/components/ClubInformationEdit.vue
@@ -39,36 +39,57 @@ export default {
     <div class="card-body">
       <h5 class="card-title">연락처</h5>
       <form @submit.prevent="submit">
-        <input
-          type="text"
-          class="form-control"
-          v-model="form.address"
-          placeholder="대강당 109호"
-        />
-        <input
-          type="email"
-          class="form-control"
-          v-model="form.email"
-          placeholder="roompennoyeah@gmail.com"
-        />
-        <input
-          type="text"
-          class="form-control"
-          v-model="form.president_name"
-          placeholder="홍길동"
-        />
-        <input
-          type="tel"
-          class="form-control"
-          v-model="form.president_tel"
-          placeholder="010-1234-5678"
-        />
-        <input
-          type="url"
-          class="form-control"
-          v-model="form.join_form_url"
-          placeholder="http://example.com"
-        />
+        <div class="form-floating">
+          <input
+            type="text"
+            class="form-control"
+            id="address"
+            v-model="form.address"
+            placeholder="대강당 109호"
+          />
+          <label for="address">주소</label>
+        </div>
+        <div class="form-floating">
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            v-model="form.email"
+            placeholder="roompennoyeah@gmail.com"
+          />
+          <label for="email">메일주소</label>
+        </div>
+        <div class="form-floating">
+          <input
+            type="text"
+            class="form-control"
+            id="president-name"
+            v-model="form.president_name"
+            placeholder="홍길동"
+          />
+          <label for="president-name">회장 실명</label>
+        </div>
+        <div class="form-floating">
+          <input
+            type="tel"
+            class="form-control"
+            id="president-tel"
+            v-model="form.president_tel"
+            placeholder="010-1234-5678"
+          />
+          <label for="president-tel">회장 전화번호</label>
+        </div>
+        <div class="form-floating">
+          <input
+            type="url"
+            class="form-control"
+            id="join-form-url"
+            v-model="form.join_form_url"
+            placeholder="http://example.com"
+          />
+          <label for="join-form-url">동아리 회원가입 주소</label>
+        </div>
+
         <button type="submit" class="btn btn-primary">게시</button>
       </form>
     </div>
