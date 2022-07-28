d7b7d53a649ffd36147441fc5e9aa063e81dafdd
trulybright <trulybright@yonsei.ac.kr>
2022-07-28 12:19:32 +0900

    문집 추가 그냥 텍스트로

--- frontend/src/components/MagazineListAction.vue
+++ frontend/src/components/MagazineListAction.vue
@@ -2,8 +2,6 @@
 import { RouterLink } from "vue-router";
 </script>
 <template>
-  <RouterLink to="/magazines/write" class="btn btn-primary"
-    >문집 추가</RouterLink
-  >
+  <RouterLink to="/magazines/write">문집 추가</RouterLink>
 </template>
 <style scoped></style>
