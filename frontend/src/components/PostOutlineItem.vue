430139978e3dd170322e5e3c148ba5dbb2827f88
trulybright <trulybright@yonsei.ac.kr>
2022-07-28 11:32:40 +0900

    fix: Prettify PostOutlineItem when Post Title is more than 1 line

--- frontend/src/components/PostOutlineItem.vue
+++ frontend/src/components/PostOutlineItem.vue
@@ -15,16 +15,16 @@ export default {
 <template>
   <div class="container">
     <div class="row text-center d-none d-lg-grid">
-      <div class="post-no">{{ no }}</div>
+      <div class="post-no align-self-center">{{ no }}</div>
       <RouterLink
         class="post-title"
         :to="{ name: 'notice', params: { no: no } }"
         >{{ title }}</RouterLink
       >
-      <div class="post-author">{{ author }}</div>
+      <div class="post-author align-self-center">{{ author }}</div>
       <small class="post-published align-self-center">{{ published }}</small>
     </div>
-    <div class="d-flex flex-row justify-content-between d-lg-none">
+    <div class="d-lg-none">
       <RouterLink :to="{ name: 'notice', params: { no: no } }">{{
         title
       }}</RouterLink>
@@ -34,12 +34,16 @@ export default {
     </div>
   </div>
 </template>
-<style scoped>
+<style scoped lang="scss">
 a {
   color: var(--color-brown) !important;
 }
 .row {
   display: grid;
   grid-template-columns: auto 1fr auto auto;
+}
+div.d-lg-none {
+  display: grid;
+  grid-template-columns: 1fr auto;
 }
 </style>
