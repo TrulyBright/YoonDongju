1939ab486a4cdb0fb27a7ecbfb69fc4f98ef8119
trulybright <trulybright@yonsei.ac.kr>
2022-09-04 20:28:36 +0900

    manually specify secret environment variables

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
 import os
 import json
+
+
 def pytest_configure(config):
-    with open("local.settings.json", encoding="utf-8") as f:
-        for key, value in json.load(f)["Values"].items():
-            os.environ[key] = value
+    if os.environ["CI"]:
+        with open("local.settings.json", encoding="utf-8") as f:
+            for key, value in json.load(f)["Values"].items():
+                os.environ[key] = value
