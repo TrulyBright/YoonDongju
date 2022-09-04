cb2e5e577536041c59f52caeb1adf1f5be70776a
trulybright <trulybright@yonsei.ac.kr>
2022-09-04 20:29:53 +0900

    fix: test not working in CI environment

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import json
 
 
 def pytest_configure(config):
-    if os.environ["CI"]:
+    if not os.environ["CI"]:
         with open("local.settings.json", encoding="utf-8") as f:
             for key, value in json.load(f)["Values"].items():
                 os.environ[key] = value
